69898bc0a87bba4a91bb18423e176d4914b86b36
kennedimalheiros <kennedimalheiros@gmail.com>
2012-06-06 18:10:51 -0300

    Mapeamento da Classe Profissao.

---
+++ SGAS/SGAS-ejb/src/java/br/com/ademoc/sgas/DomainModel/Profissao.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ademoc.sgas.DomainModel;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @version 1.0
+ * @since 1.0 06/06/2012
+ * @author Kennedi Paulo S. Malheiros
+ */
+@Entity (name="profissoes")
+public class Profissao implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column (name="codigo")
+    private Long id;
+
+    @Column (name="descricao", length=255, nullable=false)
+    private String descricao;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    //Construtor Classe
+    public void Profissao(){
+        
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Profissao)) {
+            return false;
+        }
+        Profissao other = (Profissao) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.ademoc.sgas.DomainModel.Profissao[ id=" + id + " ]";
+    }
+    
+}
