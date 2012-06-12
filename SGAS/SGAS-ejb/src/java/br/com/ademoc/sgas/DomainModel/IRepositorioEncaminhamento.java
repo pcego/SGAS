65dd7e8f7d194882d33c595ae1af4f81c8871413
kennedimalheiros <kennedimalheiros@gmail.com>
2012-06-12 10:30:49 -0300

    Criando metodos de Interfaces

--- SGAS/SGAS-ejb/src/java/br/com/ademoc/sgas/DomainModel/IRepositorioEncaminhamento.java
+++ SGAS/SGAS-ejb/src/java/br/com/ademoc/sgas/DomainModel/IRepositorioEncaminhamento.java
@@ -4,10 +4,23 @@
  */
 package br.com.ademoc.sgas.DomainModel;
 
+import java.util.Date;
+import java.util.List;
+
 /**
- *
- * @author pcego
+ * 
+ * @author Kennedi Paulo S. Malheiros
  */
-public interface IRepositorioEncaminhamento {
+public interface IRepositorioEncaminhamento extends Repositorio<Encaminhamento> {
+    
+    List<Encaminhamento> listaTodos();
+    
+    List<Encaminhamento> listaPorVaga(Vaga vaga);
+    
+    List<Encaminhamento> listaPorCliente(Cliente cliente);
+    
+    List<Encaminhamento> listaPorData(Date data);
+    
+    List<Encaminhamento> listaPorUsuario(Usuario usuario);
     
 }
