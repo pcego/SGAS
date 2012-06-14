351d640b0a9e6777ede0aa5a7b4e6ac1494eda29
kennedimalheiros <kennedimalheiros@gmail.com>
2012-06-14 19:56:04 -0300

    Implementando metodos DAOCategoria

---
+++ SGAS/SGAS-ejb/src/java/br/com/ademoc/sgas/DataAccess/DAOCategoria.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ademoc.sgas.DataAccess;
+
+import br.com.ademoc.sgas.DomainModel.Categoria;
+import br.com.ademoc.sgas.DomainModel.IRepositorioCategoria;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author www
+ */
+@Stateless
+public class DAOCategoria extends DAOGenerico<Categoria> implements IRepositorioCategoria {
+
+    public DAOCategoria() {
+        super(Categoria.class);
+    }
+
+    @Override
+    protected Long getID(Categoria obj) {
+        return obj.getId();
+    }
+
+    @Override
+    public List<Categoria> listaTodas() {
+        Query consulta = getManager().createQuery("select c from Categoria c");
+        return consulta.getResultList();
+    }
+    
+    
+    
+}
