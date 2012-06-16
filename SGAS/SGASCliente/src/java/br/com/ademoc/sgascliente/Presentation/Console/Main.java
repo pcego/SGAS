bf249da38e71358d4c9b238a02499c0db0517c72
kennedimalheiros <kennedimalheiros@gmail.com>
2012-06-15 22:21:20 -0300

    Testando Cadastro Usuario Console.

--- SGAS/SGASCliente/src/java/br/com/ademoc/sgascliente/Presentation/Console/Main.java
+++ SGAS/SGASCliente/src/java/br/com/ademoc/sgascliente/Presentation/Console/Main.java
@@ -4,6 +4,14 @@
  */
 package br.com.ademoc.sgascliente.Presentation.Console;
 
+import br.com.ademoc.sgas.DomainModel.IRepositorioUsuario;
+import br.com.ademoc.sgas.DomainModel.Usuario;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 /**
  *
  * @author pcego
@@ -14,6 +22,18 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try {
+            Context context = (Context) new InitialContext();
+            IRepositorioUsuario ejb = (IRepositorioUsuario) context.lookup("java:global/SGAS/SGAS-ejb/DAOUsuario");
+            Usuario usuario = new Usuario();
+            usuario.setNome("Kennedi Paulo");
+            usuario.setLogon("root");
+            usuario.setSenha("123");
+            usuario.setNivel(true);
+        } catch (NamingException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        
     }
 }
